71c365f248b29617dcefcfa5aee704b369db41b6
Sajid <sajidali2444@gmail.com>
2021-10-28 16:04:42 +0500

    vite-client

--- client-vite/src/App.tsx
+++ client-vite/src/App.tsx
@@ -1,44 +1,65 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import {useMemo, useState} from 'react'
+import {CssBaseline, ThemeProvider} from '@mui/material'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+import {Layout} from '@/components/Layout'
+import {PageDefault} from '@/components/PageDefault'
+
+import {AppContext, ThemeModeContext} from '@/contexts'
+import {AppClient} from '@/clients'
+import {routes} from '@/config'
+import {Route as AppRoute} from '@/types'
+import {getAppTheme} from '@/styles/theme'
+import {DARK_MODE_THEME, LIGHT_MODE_THEME} from '@/utils/constants'
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log('hi in app file')
+  const [mode, setMode] = useState<
+    typeof LIGHT_MODE_THEME | typeof DARK_MODE_THEME
+  >(DARK_MODE_THEME)
+  const appClient = new AppClient()
+
+  const themeMode = useMemo(
+    () => ({
+      toggleThemeMode: () => {
+        setMode(prevMode =>
+          prevMode === LIGHT_MODE_THEME ? DARK_MODE_THEME : LIGHT_MODE_THEME,
+        )
+      },
+    }),
+    [],
+  )
+
+  const theme = useMemo(() => getAppTheme(mode), [mode])
+
+  const addRoute = (route: AppRoute) => (
+    <Route
+      key={route.key}
+      path={route.path}
+      component={route.component || PageDefault}
+      exact
+    />
+  )
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <AppContext.Provider value={appClient}>
+      <ThemeModeContext.Provider value={themeMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <Switch>
+              <Layout>
+                {routes.map((route: AppRoute) =>
+                  route.subRoutes
+                    ? route.subRoutes.map((item: AppRoute) => addRoute(item))
+                    : addRoute(route),
+                )}
+              </Layout>
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </ThemeModeContext.Provider>
+    </AppContext.Provider>
   )
 }
 
